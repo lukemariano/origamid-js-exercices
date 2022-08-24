011e517e80eb80982f9605fcf6314df3b39049f9
Lucas Mariano <lucas.svmariano@gmail.com>
2022-08-24 14:19:16 -0300

    exercice completed

---
+++ objects.exercice/tudo-e-objeto/script.js
@@ -0,0 +1,10 @@
+// nomeie 3 propriedades ou métodos de strings
+//length, replace, toLowerCase;
+
+// nomeie 5 propriedades ou métodos de elementos do DOM
+// addEventListener, querySelector,classList, style, innerHtml
+
+// busque na web um objeto (método) capaz de interagir com o clipboard, 
+// clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
+
+// https://clipboardjs.com/#example-action
